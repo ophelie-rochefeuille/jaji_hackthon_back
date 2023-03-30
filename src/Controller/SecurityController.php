477676036922d3febe63c0746ca2d431f90069e4
Elodie Jolo <elodie.jolo@gmail.com>
2023-03-30 16:24:16 +0200

    code bis

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -14,30 +17,52 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login', methods: ['POST'])]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    /*public function login(AuthenticationUtils $authenticationUtils): Response
     {
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
 
         if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
             return $this->json([
-                'error' => 'Invalid login request: check that the Content-Type header is "application/json".'.$error
+                'error' => 'Invalid login request: check that the Content-Type header is "application/json".'//.$error
             ], 400);
         }
 
         return $this->json([
             'user' => $this->getUser() ? $this->getUser()->getId() : null,
         ]);
-        // get the login error if there is one
-        /*$error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
 
-        //return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-        return $this->json('Error');*/
+        return $this->json('Error');
+    }*/
+
+    public function login(TokenStorageInterface $tokenStorage, Request $request)
+    {
+        $user = $this->getUser();
+        if( $user ){
+
+            // Create a response object so we can attach the Remember Me cookie
+            // to be sent back to the client
+            $response = new JsonResponse( [
+                'username' => $user->getEmail(),
+                'roles' => $user->getRoles()
+            ] );
+
+
+            // Capture the JSON Payload posted from the client
+            $payload = [];
+            if( $request->getContentType() === 'json' ) {
+              $payload = json_decode($request->getContent(), true);
+
+              if (json_last_error() !== JSON_ERROR_NONE) {
+                  throw new \Exception('Invalid json: ' . json_last_error_msg() );
+              }
+            }
+
+
+            return $response;
+        }
+
+        return new JsonResponse( [
+            'message' => 'Login failed'
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
